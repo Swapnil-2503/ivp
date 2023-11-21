90f52cfe417b7b3f39539c2a90af425e8adc9d36
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2023-11-21 23:48:47 +0530

    Update first.m

--- first.m
+++ first.m
@@ -1,24 +1,20 @@
-a=imread('image.jpg');
-
+clc;
+close all;
+clear all;
+I = imread('chrysanthemum.jpg');
 figure(1);
 subplot(2,2,1);
-title("original image");
-imshow(a);
-
-b=0.3.*a(:,:,1)+0.59.*a(:,:,2)+0.11.*a(:,:,3);
-figure(2);
+imshow(I);
+title('Original Image');
+J=0.3.*I(:,:,1)+0.59.*I(:,:,2)+0.11.*I(:,:,3);
 subplot(2,2,2);
-title("by formula");
-imshow(b);
-
-c=rgb2gray(a);
-figure(3);
+imshow(J);
+title('Grayscale Image by Formula');
+K=rgb2gray(I);
 subplot(2,2,3);
-title("by function rgb2gray");
-imshow(c);
-
-d=im2bw(c);
-figure(4);
+imshow(K);
+title('Grayscale Image by rgb2gray function');
+L=im2bw(K);
 subplot(2,2,4);
-title("by function im2bw");
-imshow(d);
+imshow(L);
+title('Binary Image');
